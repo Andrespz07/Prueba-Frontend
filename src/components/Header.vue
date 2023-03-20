e37d043528ee86f3100b7120ef92c851283f61f8
Andrespz07 <andresperezortega2001@gmail.com>
2023-03-20 10:42:14 +0100

    variables.scss

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <p>Podcaster</p>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables.scss';
+
+header{
+    margin: auto;
+    border-bottom: 1px solid $Grey;
+    width: 99%;
+p{
+    color: $Blue;
+    font-weight: bold;
+    margin-bottom: 1vh;
+    font-size: 1.1rem;
+}}
+</style>
